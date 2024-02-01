f0fbe3716214af04aece49497902c665c9292b26
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-01 14:37:37 +0300

    customer, product and order models have been created

--- web_store/customers/models.py
+++ web_store/customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    registration_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Customer: {self.name}, email: {self.email}'
