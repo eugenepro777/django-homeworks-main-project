6a060d41e32f9cc091c5528b7c973ac6f5a0aa52
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-03 00:48:02 +0300

    second version for update_order.py

--- web_store/orders/management/commands/update_order.py
+++ web_store/orders/management/commands/update_order.py
@@ -4,7 +4,7 @@ from customers.models import Customer
 from products.models import Product
 
 
-# python manage.py update_order <order_id> <customer_id> <product_id_1> <product_quantity_1> <product_id_2> <product_quantity_2> ...
+# python manage.py update_order <order_id> <customer_id> <product_id_1> <product_id_2> <product_id_3> ...
 
 class Command(BaseCommand):
     help = "Update an order"
@@ -13,15 +13,16 @@ class Command(BaseCommand):
         parser.add_argument('order_id', type=int, help='ID of the order to update')
         parser.add_argument('customer_id', type=int, help='ID of the new customer')
         parser.add_argument('product_ids', type=int, nargs='+', help='IDs of the products to add to the order')
-        parser.add_argument('quantities', type=int, nargs='+', help='Quantities corresponding to the products')
+        # parser.add_argument('quantities', type=int, nargs='+', help='Quantities corresponding to the products')
 
     def handle(self, *args, **kwargs):
+        # обнуляем сумму заказа
+        total_amount = 0
         order_id = kwargs['order_id']
         customer_id = kwargs['customer_id']
         product_ids = kwargs['product_ids']
-        quantities = kwargs['quantities']
 
-        # проверяем есть ли у нас заказ и пользователь с заданным id
+        # проверяем есть ли у нас заказ и клиент с заданным id
         try:
             order = Order.objects.get(pk=order_id)
         except Order.DoesNotExist:
@@ -34,19 +35,18 @@ class Command(BaseCommand):
             return
 
         order.customer = customer
-        # обнуляем сумму заказа перед добавлением новых товаров
-        order.total_amount = 0
-        order.save()
 
+        # очищаем существующие продукты и добавляем новые по переданным id
         order.products.clear()
-        for product_id, quantity in zip(product_ids, quantities):
+        for product_id in product_ids:
             try:
                 product = Product.objects.get(pk=product_id)
+                order.products.add(product)
+                total_amount += product.price
             except Product.DoesNotExist:
                 self.stdout.write(self.style.ERROR(f'Product with ID {product_id} does not exist'))
                 return
+        order.total_amount = total_amount
+        order.save()
 
-            order.products.add(product, through_defaults={'quantity': quantity})
-            order.total_amount += product.price * quantity
-
-            self.stdout.write(self.style.ERROR(f'Successfully updated order: {order}'))
+        self.stdout.write(self.style.SUCCESS(f'Successfully updated order: {order}'))
