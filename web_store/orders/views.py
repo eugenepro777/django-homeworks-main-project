406c1b817f1422bf49d9061adb7d53a599b15ae7
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-06 13:53:57 +0300

    update views orders and products

--- web_store/orders/views.py
+++ web_store/orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Customer, Product, Order
 
 
-def fetch_orders_for_customer(request, customer_id):
+def fetch_customer_orders(request, customer_id):
     customer = get_object_or_404(Customer, pk=customer_id)
     orders = Order.objects.filter(customer=customer_id)
     context = {
@@ -10,3 +10,8 @@ def fetch_orders_for_customer(request, customer_id):
         'orders': orders,
     }
     return render(request, 'orders/orders.html', context)
+
+
+def customer_orders(request, customer_id):
+    orders = Order.objects.filter(customer_id=customer_id).prefetch_related('products')
+    return render(request, 'orders/customer_orders.html', {'customer_orders': customer_orders})
