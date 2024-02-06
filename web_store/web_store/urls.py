7b1875d6f551b9e3352c50a5764bca3bcf41c3a1
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-06 15:31:45 +0300

    added template

--- web_store/web_store/urls.py
+++ web_store/web_store/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('customers/', include('customers.urls')),
-    path('products/', include('products.urls')),
+    # path('customers/', include('customers.urls')),
+    # path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
 ]
