82ebfab994aff1bb2df3b3632b4bda25f9d36b03
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-06 13:06:26 +0300

    a view has been created to receive orders for the client

--- web_store/orders/views.py
+++ web_store/orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Customer, Product, Order
 
-# Create your views here.
+
+def fetch_orders_for_customer(request, customer_id):
+    customer = get_object_or_404(Customer, pk=customer_id)
+    orders = Order.objects.filter(customer=customer_id)
+    context = {
+        'customer': customer,
+        'orders': orders,
+    }
+    return render(request, 'orders/orders.html', context)
