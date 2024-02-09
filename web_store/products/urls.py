524adba90282d2e65010210dc6872a1ce958e92e
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-09 21:19:03 +0300

    update urls for project and products

--- web_store/products/urls.py
+++ web_store/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import add_product, fetch_product, fetch_product_list, edit_product
 
 urlpatterns = [
-    path('add_product/', add_product, name='add_product'),
-    path('edit_product/<int:product_id>/', edit_product, name='edit_product'),
-    path('product_list/', fetch_product_list, name='product_list'),
+    path('add/', add_product, name='add_product'),
+    path('edit/<int:product_id>/', edit_product, name='edit_product'),
+    path('list/', fetch_product_list, name='product_list'),
     path('<int:product_id>/', fetch_product, name='product_view'),
 ]
