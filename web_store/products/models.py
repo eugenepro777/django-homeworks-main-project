2111fa2fc85e2aa623d0987f5b6ca15443f126de
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-09 13:30:41 +0300

    update models in db

--- web_store/products/models.py
+++ web_store/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -7,7 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     added_date = models.DateField(auto_now_add=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default=timezone.now)
 
     def __str__(self):
         return f'Product: {self.name}, price: {self.price}'
