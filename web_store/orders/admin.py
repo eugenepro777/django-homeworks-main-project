6551725e14729dfbd8a494b8bb666a0e481c98af
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-11 18:56:36 +0300

    displaying order information in the admin panel

--- web_store/orders/admin.py
+++ web_store/orders/admin.py
@@ -1,3 +1,33 @@
+import csv
 from django.contrib import admin
+from django.http import HttpResponse
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'total_amount', 'order_date']
+    list_filter = ['order_date', 'customer']
+    readonly_fields = ['order_date', 'total_amount']
+    search_fields = ['id', 'total_amount']
+    search_help_text = 'Поиск товара по номеру и сумме'
+    fieldsets = [
+        ('Основная информация', {
+            'fields': ['customer', 'total_amount', 'order_date']
+        }),
+        ('Товары', {
+            'fields': ('products',)
+        })
+    ]
+    filter_horizontal = ('products',)
+
+    @admin.action(description='Экспорт в CSV')
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="orders.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['ID', 'Клиент', 'Сумма заказа', 'Дата заказа'])
+
+        for order in queryset:
+            writer.writerow([order.id, order.customer.name, order.total_amount, order.order_date])
