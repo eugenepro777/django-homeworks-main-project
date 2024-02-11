72b080c2b17d7929f8181132e5de3728b0f1207c
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-11 14:51:45 +0300

    updating models for their Russification in the admin panel

--- web_store/orders/models.py
+++ web_store/orders/models.py
@@ -4,10 +4,14 @@ from products.models import Product
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    order_date = models.DateField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Клиент')
+    products = models.ManyToManyField(Product, verbose_name='Товары')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма заказа')
+    order_date = models.DateField(auto_now_add=True, verbose_name='Дата заказа')
 
     def __str__(self):
         return f'Order: {self.id} - {self.customer.name}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
