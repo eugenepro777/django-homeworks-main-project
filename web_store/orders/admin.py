2c115548afdf77d2d355b296c6cc024720cac88b
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-11 20:09:33 +0300

    updated admin.py for orders and products

--- web_store/orders/admin.py
+++ web_store/orders/admin.py
@@ -11,7 +11,8 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ['order_date', 'customer']
     readonly_fields = ['order_date', 'total_amount']
     search_fields = ['id', 'total_amount']
-    search_help_text = 'Поиск товара по номеру и сумме'
+    search_help_text = 'Поиск заказа по номеру и сумме'
+    actions = ['export_to_csv']
     fieldsets = [
         ('Основная информация', {
             'fields': ['customer', 'total_amount', 'order_date']
@@ -22,6 +23,7 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     filter_horizontal = ('products',)
 
+    # здесь встраиваем действие 'Экспорт в CSV' внутрь класса, как его метод, modeladmin не нужен
     @admin.action(description='Экспорт в CSV')
     def export_to_csv(self, request, queryset):
         response = HttpResponse(content_type='text/csv')
@@ -31,3 +33,4 @@ class OrderAdmin(admin.ModelAdmin):
 
         for order in queryset:
             writer.writerow([order.id, order.customer.name, order.total_amount, order.order_date])
+        return response
--- web_store/products/admin.py
+++ web_store/products/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+# здесь действие 'Обнулить товарный остаток' идет отдельной задекорированной функцией, modeladmin - обязательно!
+@admin.action(description="Обнулить товарный остаток")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Для списка товаров"""
+    list_display = ['name', 'price', 'quantity']
+    list_filter = ['added_date', 'price']
+    readonly_fields = ['added_date']
+    search_fields = ['description']
+    search_help_text = 'Поиск товара по описанию'
+    actions = [reset_quantity]
+    """Для отдельного товара"""
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name']
+            }
+        ),
+        (
+            'Отдел закупок',
+            {
+                'fields': ['quantity', 'price'],
+                'description': 'Для управления товарными запасами'
+            },
+        ),
+        (
+            'Описание товара',
+            {
+                'classes': ['collapse'],
+                'description': 'Подробная информация о товаре',
+                'fields': ['description'],
+            }
+        ),
+        (
+            'Изображение товара',
+            {
+                'description': 'Изображение товара',
+                'fields': ['image'],
+            }
+        ),
+        (
+            'Прочее',
+            {
+                'description': 'Дата добавления',
+                'fields': ['added_date'],
+            }
+        ),
+    ]
