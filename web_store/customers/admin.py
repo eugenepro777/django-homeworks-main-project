a6371fe3ff934e002cde91e0b80ae7403f81c1b6
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-11 18:58:02 +0300

    update customers admin.py

--- web_store/customers/admin.py
+++ web_store/customers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Customer
 
 
@@ -6,9 +7,10 @@ from .models import Customer
 class CustomerAdmin(admin.ModelAdmin):
     """Для списка клиентов"""
     list_display = ['name', 'email', 'phone_number', 'address']
+    list_filter = ['registration_date']
     readonly_fields = ['registration_date']
-    search_fields = ['phone_number']
-    search_help_text = 'Поиск клиента по номеру телефона'
+    search_fields = ['name', 'phone_number']
+    search_help_text = 'Поиск клиента по имени и номеру телефона'
     """Для отдельного клиента"""
     fieldsets = [
         (
