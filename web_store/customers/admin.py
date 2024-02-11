fd101d1143cf9ff3153072df0ca8dfa31150cd12
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-11 15:39:26 +0300

    displaying customer information in the admin panel

--- web_store/customers/admin.py
+++ web_store/customers/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Для списка клиентов"""
+    list_display = ['name', 'email', 'phone_number', 'address']
+    readonly_fields = ['registration_date']
+    search_fields = ['phone_number']
+    search_help_text = 'Поиск клиента по номеру телефона'
+    """Для отдельного клиента"""
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name']
+            }
+        ),
+        (
+            'Контакты',
+            {
+                'description': 'Контактная информация клиента',
+                'fields': ['phone_number', 'email'],
+            },
+            'Адрес',
+            {
+                'classes': ['collapse'],
+                'description': 'Адрес для доставки',
+                'fields': ['address'],
+            }
+        ),
+    ]
