60b6c7474b86780a80acbfa9cb1249e07ece0403
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-15 13:43:46 +0300

    update web_store/urls

--- web_store/web_store/urls.py
+++ web_store/web_store/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    # path('customers/', include('customers.urls')),
+    path('customers/', include('customers.urls')),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
 ]
