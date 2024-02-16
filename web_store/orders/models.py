8e4c45dfdf57a270bd9e767512b71e3ed3d96fd8
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-16 13:35:49 +0300

    added method calculate_amount and default=0.01 for total_amount

--- web_store/orders/models.py
+++ web_store/orders/models.py
@@ -6,7 +6,7 @@ from products.models import Product
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Клиент')
     products = models.ManyToManyField(Product, verbose_name='Товары')
-    total_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name='Сумма заказа')
+    total_amount = models.DecimalField(default=0.01, max_digits=10, decimal_places=2, verbose_name='Сумма заказа')
     order_date = models.DateField(auto_now_add=True, verbose_name='Дата заказа')
 
     def __str__(self):
@@ -24,6 +24,14 @@ class Order(models.Model):
     def update_total_amount(self):
         self.total_amount = self.products.aggregate(total=models.Sum('price'))['total']
         self.save()
+
+    def calculate_amount(self):
+        amount = 0
+        for product in self.products.all():
+            amount += product.price * product.quantity
+        if self.total_amount != amount:
+            self.total_amount = amount
+        self.save(update_fields=['total_amount'])
 
     class Meta:
         verbose_name = 'Заказ'
