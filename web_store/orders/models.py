fe279e36023bd921a6f264cb11eeeec7e7bad93b
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-16 12:25:13 +0300

    update orders/models.py and migrate to db

--- web_store/orders/models.py
+++ web_store/orders/models.py
@@ -6,7 +6,7 @@ from products.models import Product
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Клиент')
     products = models.ManyToManyField(Product, verbose_name='Товары')
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма заказа')
+    total_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name='Сумма заказа')
     order_date = models.DateField(auto_now_add=True, verbose_name='Дата заказа')
 
     def __str__(self):
