2aafa86aa007739755139cd315e9ca76ce2dafc3
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-17 17:40:02 +0300

    update products/models

--- web_store/products/models.py
+++ web_store/products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     quantity = models.IntegerField(verbose_name='Количество')
     added_date = models.DateField(auto_now_add=True, verbose_name='Дата добавления')
-    image = models.ImageField(upload_to='images/', default='Add image', blank=True, verbose_name='Изображение')
+    image = models.ImageField(upload_to='images/', default='default_image.png', blank=True, verbose_name='Изображение')
 
     def __str__(self):
         return self.name
